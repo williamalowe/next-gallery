75235f437908be22c2a1cb3a839d8fc301221432
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-02 16:39:16 +1000

    feat: add page content placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl font-bold">Welcome, </h1>
+      <p className="max-w-[768px] mx-auto leading-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sapiente porro harum repudiandae doloremque adipisci unde ea neque, accusamus ipsum doloribus odit beatae tenetur distinctio esse eveniet est veniam excepturi!</p>
     </main>
   );
 }
