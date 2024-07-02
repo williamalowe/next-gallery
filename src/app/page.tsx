315d4642a42b3a28516cbef6c6195768f5fc0595
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-02 18:03:08 +1000

    styles: add link-button-component motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+import LinkButton from "@/components/link-button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="text-center pt-32 px-5">
-      <h1 className="text-4xl font-bold">Welcome, </h1>
+    <main className="text-center pt-8 px-5 flex flex-col items-center gap-y-4">
+      <div className="flex items-center">
+        <div>
+          <p>Hi, I&apos;m</p>
+          <h1 className="text-4xl font-bold">William Lowe</h1>
+        </div>
+        <div>
+          <Image 
+            className="w-[300px] h-[300px] object-cover grayscale rounded-full scale-x-[-1]"
+              src="/profile-removebg.png"
+              alt="profile picture"
+              width="300"
+              height="300"
+          />
+        </div>
+      </div>
       <p className="max-w-[768px] mx-auto leading-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sapiente porro harum repudiandae doloremque adipisci unde ea neque, accusamus ipsum doloribus odit beatae tenetur distinctio esse eveniet est veniam excepturi!</p>
+      <LinkButton 
+        href="/gallery"
+        text="See my work"
+      />
     </main>
   );
 }
---
+++ src/components/link-button.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import React from "react";
+
+export default function LinkButton({
+  href,
+  text,
+}: {
+  href: string;
+  text: string;
+}) {
+  return (
+    <Link href={href}>
+      <motion.div
+        className="w-[148px] flex gap-x-2 items-center p-2 bg-zinc-900 text-white font-bold rounded-md"
+        whileHover={{
+          scale: 1.1,
+        }}
+        whileTap={{
+          scale: 0.9,
+        }}
+      >
+        {text}
+        <motion.div
+          initial={{
+            x: 0,
+          }}
+          animate={{
+            x: 10,
+          }}
+          transition={{
+            duration: 1,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowRightLong} />
+        </motion.div>
+      </motion.div>
+    </Link>
+  );
+}
